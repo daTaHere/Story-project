1dd59c6c937ae5b42d5ee2c1022d06fe89464d66
Adam Huynh <127999910+daTaHere@users.noreply.github.com>
2023-07-24 21:08:34 -0700

    SharedStory model

---
+++ SharedStory.cs
@@ -0,0 +1,24 @@
+﻿using Sabio.Models.Domain.Users;
+using Sabio.Models.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.SharedStories
+{
+    public class SharedStory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Story { get; set; }
+        public User CreatedBy { get; set; }
+        public File File { get; set; }
+        public bool IsApproved { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
